74d6080d01fb426bcc3d6393e37a27e4b58fc0f2
David Dias <ddiasrocha@hotmail.com>
2024-09-16 04:50:39 -0300

    Product

--- src/main/java/com/educandoweb/course/resources/ProductResources.java
+++ src/main/java/com/educandoweb/course/resources/ProductResources.java
@@ -1,7 +1,7 @@
 package com.educandoweb.course.resources;
 
-import com.educandoweb.course.entities.Category;
-import com.educandoweb.course.services.CategoryService;
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,22 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/categories")
-public class CategoryResource {
+@RequestMapping(value = "/products")
+public class ProductResources {
 
     @Autowired
-    private CategoryService service;
+    private ProductService service;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
-        //Category u = new Category(1L, "Maria", "maria@gmail.com", "999999999", "12345");
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<Product>> findAll(){
+        List<Product> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Category> findById(@PathVariable Long id){
-        Category obj = service.findById(id);
+    public ResponseEntity<Product> findById(@PathVariable Long id){
+        Product obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
--- src/main/java/com/educandoweb/course/services/CategoryService.java
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -1,6 +1,6 @@
 package com.educandoweb.course.services;
 
-import com.educandoweb.course.entities.enums.Category;
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
